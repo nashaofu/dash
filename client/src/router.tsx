af235ba4d2c1a9724e1cea279ce2ba0b1983652a
nashaofu <diaocheng@outlook.com>
2023-05-06 09:36:59 +0800

    feat: 添加 react ErrorBoundary

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Root from './routes/Root';
 import Login from './routes/Login';
 import NotFound from './routes/NotFound';
+import ErrorElement from './components/ErrorElement';
 
 const Home = lazy(() => import('./routes/Home'));
 const Apps = lazy(() => import('./routes/Apps'));
@@ -13,32 +14,39 @@ export default createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorElement message="页面加载失败" />,
     children: [
       {
         path: '/',
         index: true,
         element: <Home />,
+        errorElement: <ErrorElement message="页面加载失败" />,
       },
       {
         path: '/apps',
         element: <Apps />,
+        errorElement: <ErrorElement message="页面加载失败" />,
       },
       {
         path: '/users',
         element: <Users />,
+        errorElement: <ErrorElement message="页面加载失败" />,
       },
       {
         path: '/settings',
         element: <Settings />,
+        errorElement: <ErrorElement message="页面加载失败" />,
       },
     ],
   },
   {
     path: '/login',
     element: <Login />,
+    errorElement: <ErrorElement message="页面加载失败" />,
   },
   {
     path: '*',
     element: <NotFound />,
+    errorElement: <ErrorElement message="页面加载失败" />,
   },
 ]);
